4b8f61a805e302317bae180837bb5723b229da0b
Éverton M. Vieira <everton.muvi@gmail.com>
2022-04-30 01:01:29 -0300

    prototype development

--- portuguese.c
+++ portuguese.c
@@ -1,7 +1,19 @@
 #include <locale.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
   setlocale(LC_ALL, "pt_br.UTF-8");
-  printf("Uma palavra com cedilha e til: Elevação\n");
+  printf("Teste 1:\n");
+  printf("Impressão de uma palavra com cedilha e til:\n");
+  printf("Elevação\n");
+  FILE *file = fopen("portuguese.txt", "w");
+  if (file == NULL) {
+    printf("Error! Could not open the file\n");
+    exit(1);
+  }
+  fprintf(file, "%s\n", "Teste 1:");
+  fprintf(file, "%s\n", "Impressão de uma palavra com cedilha e til:");
+  fprintf(file, "%s\n", "Elevação");
+  fclose(file);
 }
